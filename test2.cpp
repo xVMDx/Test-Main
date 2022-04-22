0872de5ad8b00c4fb07761ea937f087542de589e
VMD <11410120210729@stud.cu.edu.eg>
2022-04-22 17:44:06 +0200

    add namespace

--- test2.cpp
+++ test2.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
-
+using namespace std;
 int main() {
 
-	std::cout << "hi\n";
-	std::cout << "hi\n";
-	std::cout << "hi\n";
+	cout << "hi\n";
+	cout << "hi\n";
+	cout << "hi\n";
 
 	
 
